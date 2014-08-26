2b5068c05769b7db7620a731a98a891d29fde5af
alaughlin <adamlaughlin@outlook.com>
2014-08-26 17:56:54 -0400

    Can add cards to boards.

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,4 +4,28 @@ class CardsController < ApplicationController
     @board = @card.board
     @user = @card.user
   end
+
+  def new
+    @card = Card.new
+    @board = Board.find(params[:board_id])
+  end
+
+  def create
+    @card = Card.new(card_params)
+    @card.user_id = current_user.id
+
+    if @card.save
+      redirect_to user_url(current_user)
+    else
+      @card = Card.new
+      flash[:errors] = @card.errors.full_messages
+      render :new
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:title, :img_url, :description, :board_id)
+  end
 end
