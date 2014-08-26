4acc2d524a9c0e2b56751905147612e146cccfd7
alaughlin <adamlaughlin@outlook.com>
2014-08-26 10:29:59 -0400

    First commit.

---
+++ app/models/user.rb
@@ -0,0 +1,37 @@
+class User < ActiveRecord::Base
+  validates :username, uniqueness: true
+  validates :username, :password_digest, presence: true
+  validates :password, length: {minimum: 6}. allow_nil: true
+
+  attr_reader :password
+
+  def reset_session_token!
+    self.session_token = self.generate_session_token
+    self.save
+
+    self.session_token
+  end
+
+  def self.generate_session_token
+    SecureRandom::urlsafe_base64
+  end
+
+  def ensure_session_token
+    self.session_token ||= User.generate_session_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def has_password?(password)
+    pw = BCrypt::Password.new(self.password_digest)
+    pw.is_password?(password)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.has_password?(password) ? user : nil
+  end
+end
