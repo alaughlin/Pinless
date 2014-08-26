e2071f3fa410e126bf007c88c7ca648252e83a3a
alaughlin <adamlaughlin@outlook.com>
2014-08-26 14:04:56 -0400

    Nested shallow routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   resources :users do
-    resources :cards
+    resources :boards , shallow: true do
+      resources :cards, shallow: true
+    end
   end
   resource :session, only: [:new, :create, :destroy]
 end
