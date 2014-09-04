b9913136f138c1ac94222cca298e3dfdcc11fc5a
alaughlin <adamlaughlin@outlook.com>
2014-09-04 11:18:31 -0400

    Finally, logic on where to go whether you're signed in or not.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,4 +1,12 @@
 class StaticController < ApplicationController
+  before_filter :check_if_signed_in
+  
   def root
+  end
+
+  private
+
+  def check_if_signed_in
+    redirect_to new_session_url unless signed_in?
   end
 end
