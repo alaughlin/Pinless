62e031d6c7dc56fcbf8e994fcd56575fa25405f7
alaughlin <adamlaughlin@outlook.com>
2014-09-19 20:24:31 -0400

    feed and board urls working.

--- app/assets/javascripts/pinless.js
+++ app/assets/javascripts/pinless.js
@@ -1,10 +1,30 @@
 (function () {
   var pinlessApp = angular.module('pinlessApp', ['ngRoute']);
+  pinlessApp.value('currentUserId', window.currentUserId);
 
   pinlessApp.config(function ($routeProvider) {
     $routeProvider
       .when('/', {
-        templateUrl: 'pages/index.html'
-      })  
+        templateUrl: 'pages/index.html',
+        controller: 'FeedCtrl'
+      })
+      .when('/boards/:id', {
+        templateUrl: 'pages/boards/show.html',
+        controller: 'BoardShowCtrl'
+      })
+  });
+
+  pinlessApp.controller('FeedCtrl', function ($scope, $http) {
+    $http.get('/api/feed').success(function (data) {
+      $scope.cards = data;
+    });
+  });
+
+  pinlessApp.controller('BoardShowCtrl', function ($scope, $routeParams, $http) {
+    var route = '/api/boards/' + $routeParams.id;
+    $http.get(route).success(function (data) {
+      $scope.board = data;
+      $scope.cards = $scope.board.cards;
+    });
   });
 })();
